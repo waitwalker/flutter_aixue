cfc6d35a0e942528b28924f0b6ed280046ab91a8
waitwalker <waitwalker@163.com>
2019-09-03 13:47:21 +0800

    教师学资源课件添加

---
+++ lib/pages/teacher_resource_task_detail.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+
+
+class TeacherResourceTaskDetailPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TeacherResourceTaskDetailState();
+  }
+}
+
+class _TeacherResourceTaskDetailState extends State<TeacherResourceTaskDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return null;
+  }
+}
