4ad58bcc0967376d6288b121043eb5098b9d2ef8
waitwalker <waitwalker@163.com>
2019-10-12 13:50:21 +0800

    item 组件添加

--- lib/pages/teacher_class_notice_list_page.dart
+++ lib/pages/teacher_class_notice_list_page.dart
@@ -265,7 +265,7 @@ class _TeacherClassNoticeListState extends State<TeacherClassNoticeListPage> {
                 itemCount: classNoticeList.length,
                 mainAxisSpacing: 0,
                 crossAxisSpacing: 0,
-                itemBuilder: _taskItemBuilder,
+                itemBuilder: _itemBuilder,
                 staggeredTileBuilder: (int index){
                   return StaggeredTile.fit(2);
                 },
@@ -274,6 +274,167 @@ class _TeacherClassNoticeListState extends State<TeacherClassNoticeListPage> {
           ),
         ],
       ),
+    );
+  }
+
+  Widget _itemBuilder(BuildContext context, int index) {
+    LastTaskList lastTask = lastTaskList[index];
+    return GestureDetector(
+      child: Padding(
+        padding: EdgeInsets.only(left: 20,right: 20,top: 20,bottom: 30),
+        child:Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5.0),
+            boxShadow: [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.2),spreadRadius: 3,blurRadius: 3,offset: Offset(0, 3))],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.only(left: 20,top: 15),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 40,
+                      width: 40,
+                      child: Icon(Icons.book,color: Colors.white,),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [BoxShadow(color: Color.fromRGBO(0, 0, 0, 0.2),spreadRadius: 2,blurRadius: 2,offset: Offset(0, 2))],
+                        color: Colors.lightBlue,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 20)),
+                    Container(
+                      width: Platform.isIOS ? 220 : 250,
+                      child: Text(
+                        lastTask.taskName,
+                        style: TextStyle(fontSize: 15),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Padding(padding: EdgeInsets.only(top: 50,left: 20,bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(lastTask.dateHint,style: TextStyle(fontSize: 13,color: Colors.grey),),
+
+                    Row(
+                      children: <Widget>[
+                        Text(lastTask.scaleHint,style: TextStyle(fontSize: 13,color: Colors.grey),),
+                        Padding(padding: EdgeInsets.only(left: 10,right: 20),
+                          child: Icon(Icons.account_box,size: 20,color: Colors.grey,),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      /// 任务详情路由
+      onTap: (){
+        switch (lastTask.kTaskSubtype) {
+          case ETTTaskSubtype.ETTTaskSubtypeResourceStudy:
+          /// pad上教师发布的微课任务就是学资源任务中的视频任务
+            print("学资源任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherResourceTaskDetailPage(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeDiscussion:
+            print("讨论任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherDiscussionTaskDetail(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeWebviewObjectiveItem:
+            print("单选多选等任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherPaperItemTaskDetail(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypePaperTest:
+          /// 这里包括测验任务
+            print("成卷测试任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherPaperTaskDetail(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeAutonomyTest:
+          /// 这种任务已经取消
+            print("自主测试任务");
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeMicroCourse:
+            print("微课程任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherMicroCoursePage(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeRegularTaskVoice:
+            print("一般任务语音");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherGeneralTaskDetailPage(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeRegularTaskPicture:
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherGeneralTaskDetailPage(lastTask);
+            }));
+            print("一般任务图片");
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeRegularTaskText:
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherGeneralTaskDetailPage(lastTask);
+            }));
+            print("一般任务文本");
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeLiveCourse:
+            print("直播任务");
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeWebviewSubjectiveItem:
+            print("主观题任务");
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherPaperItemTaskDetail(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSubtypeKnowledgeGuidance:
+
+            print("知识导学任务");
+            break;
+          case ETTTaskSubtype.ETTTaskAnswerSheet:
+            print("答题卡任务");
+            break;
+          case ETTTaskSubtype.ETTTaskAISingle:
+          /// 已取消
+            print("AI单项任务");
+            break;
+          case ETTTaskSubtype.ETTTaskAIStudyPlan:
+          /// 已取消
+            print("AI学习计划任务");
+            break;
+          case ETTTaskSubtype.ETTTaskHoneycomb:
+          /// 蜂巢任务
+            print("蜂巢任务");
+
+            Navigator.push(context, MaterialPageRoute(builder: (context){
+              return TeacherPersonalizedTaskDetail(lastTask);
+            }));
+            break;
+          case ETTTaskSubtype.ETTTaskSingSound:
+            print("先声任务");
+            break;
+        }
+      },
     );
   }
 
