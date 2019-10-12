2fc82e2eb6334525811501a422f1ceda86440da7
waitwalker <waitwalker@163.com>
2019-10-12 11:20:36 +0800

    班级通知列表逻辑完成

---
+++ lib/pages/teacher_class_notice_list_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_aixue/common/color/color.dart';
+
+///
+/// @name TeacherClassNoticeListPage
+/// @description 教师班级通知列表页面
+/// @author lca
+/// @date 2019-10-12
+///
+class TeacherClassNoticeListPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TeacherClassNoticeListState();
+  }
+}
+
+class _TeacherClassNoticeListState extends State<TeacherClassNoticeListPage> {
+
+  Future future;
+
+  @override
+  Widget build(BuildContext context) {
+    return _createFutureBuilder();
+  }
+
+  ///
+  /// @name _createFutureBuilder
+  /// @description 构建FutureBuilder
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  _createFutureBuilder() {
+    return FutureBuilder(
+      builder: _futureBuilder,
+      future: future,
+    );
+  }
+
+  ///
+  /// @name _futureBuilder
+  /// @description future控件
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  Widget _futureBuilder(BuildContext context, AsyncSnapshot snapshot) {
+    switch (snapshot.connectionState) {
+      case ConnectionState.none:
+        return _noneChild();
+      case ConnectionState.active:
+        return _activeChild();
+      case ConnectionState.waiting:
+        return _activeChild();
+      case ConnectionState.done:
+        if (snapshot.hasError) {
+          return _errorChild();
+        }
+        return _normalChild();
+      default :
+        return _activeChild();
+    }
+  }
+
+  ///
+  /// @name _noneChild
+  /// @description 没有数据控件站位
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  Widget _noneChild() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("班级通知列表"),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: IconButton(icon: Icon(Icons.data_usage),onPressed: (){
+              print("单题任务页面 统计按钮");
+            },),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("暂无数据",style: TextStyle(fontSize: 30,color: ETTColor.c1_color),),
+      ),
+    );
+  }
+
+  ///
+  /// @name _activeChild
+  /// @description 加载中控件站位
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  Widget _activeChild() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("班级通知列表"),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: IconButton(icon: Icon(Icons.data_usage),onPressed: (){
+              print("单题任务页面 统计按钮");
+            },),
+          ),
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  ///
+  /// @name _errorChild
+  /// @description 没有数据控件站位
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  Widget _errorChild() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("班级通知列表"),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: IconButton(icon: Icon(Icons.data_usage),onPressed: (){
+              print("单题任务页面 统计按钮");
+            },),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Text("遇到错误",style: TextStyle(fontSize: 30,color: ETTColor.c1_color),),
+      ),
+    );
+  }
+
+  ///
+  /// @name _normalChild
+  /// @description 请求正常站位
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-12
+  ///
+  Widget _normalChild() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("班级通知列表"),
+        leading: GestureDetector(
+          child: Icon(Icons.arrow_back_ios),
+          onTap: (){
+            Navigator.pop(context);
+          },
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20),
+            child: IconButton(icon: Icon(Icons.data_usage),onPressed: (){
+              print("单题任务页面 统计按钮");
+            },),
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("请求正常"),
+        ],
+      ),
+    );
+  }
+
+}
