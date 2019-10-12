4f6a1a3dcd2d0a9a8338e096b098d2b1da35ec1c
waitwalker <waitwalker@163.com>
2019-10-12 16:53:48 +0800

    教师发布班级通知页面创建完成

---
+++ lib/pages/teacher_publish_class_notice_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TeacherPublishClassNoticePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TeacherPublishClassNoticeState();
+  }
+}
+
+class _TeacherPublishClassNoticeState extends State<TeacherPublishClassNoticePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
