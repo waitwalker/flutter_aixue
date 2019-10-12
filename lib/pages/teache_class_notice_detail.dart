a9c789b4f0d80a147ffa19cbe320750b9e52e55f
waitwalker <waitwalker@163.com>
2019-10-12 15:16:16 +0800

    班级通知详情类创建

---
+++ lib/pages/teache_class_notice_detail.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+///
+/// @name TeacherClassNoticeDetail
+/// @description 教师班级通知详情添加
+/// @author lca
+/// @date 2019-10-12
+///
+class TeacherClassNoticeDetail extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TeacherClassNoticeDetailState();
+  }
+}
+
+class _TeacherClassNoticeDetailState extends State<TeacherClassNoticeDetail> {
+  @override
+  Widget build(BuildContext context) {
+
+    return null;
+  }
+}
