b45c8fdda9d7128ebfc29612e7ac031c7658ab6b
waitwalker <waitwalker@163.com>
2019-10-28 17:25:32 +0800

    登录助手管理类

--- lib/pages/login_register/app_login_manager.dart
+++ lib/pages/login_register/app_login_manager.dart
@@ -8,6 +8,27 @@ import 'package:flutter_aixue/assistant/enum_assistant.dart';
 ///
 class AppLoginManager {
 
+  ///
+  /// @name instance
+  /// @description 单例模式
+  /// @parameters
+  /// @return
+  /// @author lca
+  /// @date 2019-10-28
+  ///
+  factory AppLoginManager() => _getInstance();
+  static AppLoginManager get instance => AppLoginManager._getInstance();
+  static AppLoginManager _instance;
+  AppLoginManager._internal() {
+    // 初始化
+  }
+  static AppLoginManager _getInstance() {
+    if (_instance == null) {
+      _instance = new AppLoginManager._internal();
+    }
+    return _instance;
+  }
+
   static login(String account, String password, ETTLoginType loginType) {
 
   }
