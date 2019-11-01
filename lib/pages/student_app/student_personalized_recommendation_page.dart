a3164e37230ede364dbe0fab59a6da809da55157
waitwalker <waitwalker@163.com>
2019-11-01 14:50:40 +0800

    个性化推荐

--- lib/pages/student_app/student_personalized_recommendation_page.dart
+++ lib/pages/student_app/student_personalized_recommendation_page.dart
@@ -3,25 +3,25 @@ import 'package:flutter_aixue/common/redux/app_state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 ///
-/// @name StudentSelfStudyPage
-/// @description 学生自学
+/// @name StudentPersonalizedRecommendationPage
+/// @description 学生个性化推荐
 /// @author lca
 /// @date 2019-11-01
 ///
-class StudentSelfStudyPage extends StatefulWidget {
+class StudentPersonalizedRecommendationPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _StudentSelfStudyState();
+    return _StudentPersonalizedRecommendationState();
   }
 }
 
-class _StudentSelfStudyState extends State<StudentSelfStudyPage> {
+class _StudentPersonalizedRecommendationState extends State<StudentPersonalizedRecommendationPage> {
   @override
   Widget build(BuildContext context) {
     return StoreBuilder<AppState>(builder: (context, store) {
       return Scaffold(
         appBar: AppBar(
-          title: Text("自学"),
+          title: Text("个性化推荐"),
           leading: GestureDetector(
             child: Icon(Icons.arrow_back_ios),
             onTap: (){
