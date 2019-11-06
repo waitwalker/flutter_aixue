a55146af14bc7e0b3ffeb5fa651068fd269da5bf
waitwalker <waitwalker@163.com>
2019-11-06 15:54:45 +0800

    添加学生内容页面

---
+++ lib/pages/student_app/student_home_content_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class StudentHomeContentPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _StudentHomeContentState();
+  }
+}
+
+class _StudentHomeContentState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
